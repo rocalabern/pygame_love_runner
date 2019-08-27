176a84a43990a875c5cfca9d7431c21a79e5cc92
Roc Alabern <roc.alabern@gmail.com>
2019-08-28 00:33:11 +0200

    Added titles

--- love_runner.py
+++ love_runner.py
@@ -8,6 +8,7 @@ from levels.tutorial_levels import *
 from levels.game_levels import *
 from levels.test_levels import *
 
+
 def main():
     pygame.mixer.pre_init(48000, -16, 2, 2048)
     pygame.init()
@@ -28,6 +29,79 @@ def main():
     for i in range(1, len(levels)):
         level = levels[i-1]
         level.add_caption(create_caption("Level " + str(i).zfill(2), 5, 5, color_fg=(170, 170, 170)))
+
+    # music_file = 'music/8-bit-mario-theme.mp3'
+    # pygame.mixer.music.load(music_file)
+    # pygame.mixer.music.play(-1)
+    failure_sound = pygame.mixer.Sound("sounds/sad_trombone.wav")
+    success_sound = pygame.mixer.Sound("sounds/applause2.wav")
+
+    image_file_lode = 'images/titles/LodeRunner_bg.png'
+    image_file_love = 'images/titles/LodeRunner_love.png'
+    image_file_ball = 'images/titles/LodeRunner_ball_25px.png'
+
+    up = down = False
+    failed = False
+    success = False
+    state = 1
+    press_enter = 0
+    done = False
+    while not done:
+
+        # e: event
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                raise SystemExit("ESCAPE")
+            if e.type == KEYDOWN and e.key == K_ESCAPE:
+                raise SystemExit("ESCAPE")
+            if e.type == KEYDOWN and e.key == K_F2:
+                done = True
+            if e.type == KEYDOWN and e.key == K_F3:
+                done = True
+
+            if e.type == KEYDOWN or e.type == KEYUP:
+
+                if e.key == K_UP:
+                    up = e.type == KEYDOWN
+                if e.key == K_DOWN:
+                    down = e.type == KEYDOWN
+
+            if e.type == KEYDOWN:
+                if e.key == K_RETURN or e.key == K_SPACE:
+                    press_enter = 1
+
+            if down:
+                state = state + 1
+                state = min(state, 3)
+
+            if up:
+                state = state - 1
+                state = max(state, 1)
+
+            if state == 1 and press_enter == 1:
+                press_enter = 0
+                failure_sound.play()
+                failed = True
+
+            if state == 2 and failed and press_enter == 1:
+                press_enter = 0
+                success = True
+
+            if success:
+                image_file = image_file_love
+                success_sound.play()
+                done = True
+            else:
+                image_file = image_file_lode
+
+            # draw background
+            image_title = pygame.image.load(image_file)
+            image_ball = pygame.image.load(image_file_ball)
+            screen.blit(image_title, (0, 0))
+            screen.blit(image_ball, (516, 321+(state-1)*52))
+            pygame.display.update()
+            clock.tick(600)
+    pygame.time.wait(5000)
 
     done = False
     i = 0
